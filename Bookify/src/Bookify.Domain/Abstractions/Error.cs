ff3f1f25e63bbfeff6d30035705e0bd079342ea8
Howard Seraus <howard@forqstudio.nl>
2024-05-09 16:13:28 +0200

    chore: add domain layer. domain-objects, value-objects, domain-events, domain-service, repository-interfaces, unit-of-work, errors and results

---
+++ Bookify/src/Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Abstractions
+{
+    public record Error(string Code, string name)
+    {
+        public static Error None => new Error(string.Empty, string.Empty);
+        public static Error NullValue = new("Error.NullValue", "null value was provided");
+    }
+}
