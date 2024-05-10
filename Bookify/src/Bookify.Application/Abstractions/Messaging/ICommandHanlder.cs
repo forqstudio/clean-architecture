a0b2c61a80b1cc0e88e81a1237ea3a3ccb2b7ae5
Howard Seraus <howard@forqstudio.nl>
2024-05-10 09:44:36 +0200

    chore: add application layer. query-handlers, command-handlers, logging-decorator, validation-decorator

---
+++ Bookify/src/Bookify.Application/Abstractions/Messaging/ICommandHanlder.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface ICommandHanlder<TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
+{
+}
+
+public interface ICommandHanlder<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
+
