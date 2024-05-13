a60259360526dfc6f85f5a678324abc4c6f52b2c
Howard Seraus <howard@forqstudio.nl>
2024-05-13 09:29:39 +0200

    feat: add permission based authorization

---
+++ Bookify/src/Bookify.Infrastructure/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations
+{
+    internal sealed class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.ToTable("Permissions");
+
+            builder.HasKey(p => p.Id);
+
+            builder.HasData(Permission.UsersRead);
+        }
+    }
+}
