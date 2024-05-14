1ef379b6835c8e314c638c7d343e334c55452c97
Howard Seraus <howard@forqstudio.nl>
2024-05-14 15:07:44 +0200

    chore: add api-versioning with openApi integration. example versioning in user controller

--- Bookify/src/Bookify.Api/Controllers/Users/UsersController.cs
+++ Bookify/src/Bookify.Api/Controllers/Users/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Application.Users.GetLoggedInUser;
+﻿using Asp.Versioning;
+using Bookify.Application.Users.GetLoggedInUser;
 using Bookify.Application.Users.LogInUser;
 using Bookify.Application.Users.RegisterUser;
 using Bookify.Domain.Users;
@@ -10,8 +11,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace Bookify.Api.Controllers.Users;
 
 [ApiController]
-[Authorize]
-[Route("api/v1/users")]
+[ApiVersion(ApiVersions.V1)]
+[ApiVersion(ApiVersions.V2)]
+[Route("api/v{version:apiVersion}/users")]
 public class UsersController : ControllerBase
 {
     private readonly ISender _sender;
@@ -62,12 +64,25 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet("me")]
+    [ApiVersion(ApiVersions.V1)]
     [HasPermission(Permissions.UsersRead)]
-    public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellation)
+    public async Task<IActionResult> GetLoggedInUserV1(CancellationToken cancellationToken)
     {
         var query = new GetLoggedInUserQuery();
 
-        var result = await _sender.Send(query, cancellation);
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("me")]
+    [MapToApiVersion(ApiVersions.V2)]
+    [HasPermission(Permissions.UsersRead)]
+    public async Task<IActionResult> GetLoggedInUserV2(CancellationToken cancellationToken)
+    {
+        var query = new GetLoggedInUserQuery();
+
+        var result = await _sender.Send(query, cancellationToken);
 
         return Ok(result.Value);
     }
